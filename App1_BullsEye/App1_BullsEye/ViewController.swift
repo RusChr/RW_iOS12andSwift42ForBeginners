20354467e3d86951f507b8186e291852c22e1ec6
Rus Chr <r.chergizbiev@gmail.com>
2021-07-18 21:20:20 +0300

    App1_BullsEye: less 7, 0 min

--- App1_BullsEye/App1_BullsEye/ViewController.swift
+++ App1_BullsEye/App1_BullsEye/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var currentValue: Int = 0
+    
+    @IBOutlet weak var slider: UISlider!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        currentValue = Int(slider.value)
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    @IBAction func ShowAler() {
+        let alert = UIAlertController(title: "Hello, World!", message: "The value of the slider is now: \(currentValue)", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Ok", style: .default, handler: nil)
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        print("The value of the slider is now: \(currentValue)")
+        currentValue = Int(slider.value)
+    }
 
 }
 
